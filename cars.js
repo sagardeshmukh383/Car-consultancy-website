bf354211bef62abbf92e46a6d27cc3274e3a13b3
sagardeshmukh383 <32060807+sagardeshmukh383@users.noreply.github.com>
2017-09-18 22:50:48 +0530

    Add files via upload

---
+++ cars.js
@@ -0,0 +1,16 @@
+<script="text/javascript">
+function DropDown(el) {
+    this.dd = el;
+    this.initEvents();
+}
+DropDown.prototype = {
+    initEvents : function() {
+        var obj = this;
+
+        obj.dd.on('click', function(event){
+            $(this).toggleClass('active');
+            event.stopPropagation();
+        }); 
+    }
+}
+</script>
